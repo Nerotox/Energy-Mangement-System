a87c6c072856fbdb4bc461885592e19d6eb7ee38
Alexander Karrer <alex.kay.rs@gmail.com>
2019-06-15 22:06:15 +0200

    Finished first version of the EMS Server

--- EMS/src/ems/Main.java
+++ EMS/src/ems/Main.java
@@ -5,6 +5,13 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Pattern;
@@ -29,6 +36,8 @@ import model.Item;
 public class Main {
 
 	public static void main(String[] args) {
+
+		Connection dbCon = null;
 		try {
 			System.out.println("Starting the Energy Management System Server Version 1.0.");
 
@@ -84,6 +93,7 @@ public class Main {
 
 			if (status != 200) {
 				System.out.println("Something went wrong with the request. Statuscode " + status + ".");
+				return;
 			}
 
 			BufferedReader responseReader = new BufferedReader(new InputStreamReader(con.getInputStream()));
@@ -109,11 +119,86 @@ public class Main {
 				}
 			}
 			System.out.println("Found " + energyItems.size() + " Energy Items.");
-			System.out.println(gson.toJson(energyItems));
+			// System.out.println(gson.toJson(energyItems));
+
+			// SETUP SQLITE STUFF HERE
+			System.out.println("Connecting to the SQLite Database...");
+
+			String dbUrl = "jdbc:sqlite:ems.db";
+			dbCon = DriverManager.getConnection(dbUrl);
+			System.out.println("Connection established.");
+			System.out.println("Creating Table if first start...");
+			String sql = "CREATE TABLE IF NOT EXISTS Energydata(label text NOT NULL," + " value REAL NOT NULL,"
+					+ " unit text NOT NULL," + " date TEXT NOT NULL);";
+			Statement dbStatement = dbCon.createStatement();
+			dbStatement.execute(sql);
+
+			System.out.println("Created/Found the Table.");
+
+			System.out.println("Starting to poll energy data from OpenHAB...");
+
+			// Creating the loop for getting the new data and writing it to the database.
+			while (true) {
+				for (Item item : energyItems) {
+					URL itemUrl = new URL(item.link);
+					HttpURLConnection itemCon = (HttpURLConnection) itemUrl.openConnection();
+					itemCon.setRequestMethod("GET");
+					int itemStatus = itemCon.getResponseCode();
+
+					if (itemStatus != 200) {
+						System.out.println("Something went wrong with the request. Statuscode " + status + ".");
+					}
+
+					BufferedReader itemResponseReader = new BufferedReader(
+							new InputStreamReader(itemCon.getInputStream()));
+					StringBuilder itemSB = new StringBuilder();
+					String itemResponseLine;
+					while ((itemResponseLine = itemResponseReader.readLine()) != null) {
+						itemSB.append(itemResponseLine + "\n");
+					}
+					itemResponseReader.close();
+					String itemResponse = itemSB.toString();
+					Gson itemGson = new GsonBuilder().setPrettyPrinting().create();
+					Item dbItem = itemGson.fromJson(itemResponse, Item.class);
+
+					// if (dbItem.label.equalsIgnoreCase("Trockner Leistung aktuell")) {
+					// System.out.println("[" +
+					// LocalTime.now().format(DateTimeFormatter.ofPattern("HH:mm:ss"))
+					// + "] Name: " + dbItem.name + " Current energy: " + dbItem.state + " kWh");
+					// }
+					insert(dbItem, dbCon);
+					// insert item into db here
+				}
+				Thread.sleep(2500);
+			}
 		} catch (Exception e) {
 			e.printStackTrace();
+		} finally {
+			try {
+				if (dbCon != null) {
+					dbCon.close();
+				}
+			} catch (SQLException e2) {
+			}
 		}
 
+	}
+
+	private static void insert(Item item, Connection con) throws SQLException {
+		System.out.println("[" + LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))
+				+ "] Name: " + item.label + " Current energy: " + item.state + " "
+				+ item.stateDescription.pattern.split(" ")[1]);
+		String sql = "INSERT INTO Energydata(label, value, unit, date) VALUES(?,?,?,?)";
+		PreparedStatement pstmt = con.prepareStatement(sql);
+		pstmt.setString(1, item.label);
+		if (item.state.equalsIgnoreCase("NULL")) {
+			pstmt.setDouble(2, 0.0);
+		} else {
+			pstmt.setDouble(2, Double.parseDouble(item.state));
+		}
+		pstmt.setString(3, item.stateDescription.pattern.split(" ")[1]);
+		pstmt.setString(4, LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
+		pstmt.executeUpdate();
 	}
 
 	private static final Pattern PATTERN = Pattern.compile("^"
